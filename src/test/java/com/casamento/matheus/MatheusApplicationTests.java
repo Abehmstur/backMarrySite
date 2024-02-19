967782bddec0d56f12cd8785dd55a60b271f4241
Flamerson De Andrade <68919243+Flamerson@users.noreply.github.com>
2024-02-19 20:29:15 -0300

    Project init

---
+++ src/test/java/com/casamento/matheus/MatheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casamento.matheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
