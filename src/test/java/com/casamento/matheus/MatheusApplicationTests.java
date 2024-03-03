896b3e79b0b0cd46faf3c2ba3912755fe5b0b8cc
Matheus Barros <matheus.barros@esig.com.br>
2024-03-02 23:51:18 -0300

    Ajuste Testes

---
+++ src/test/java/com/casamento/matheus/MatheusApplicationTests.java
@@ -0,0 +1,9 @@
+package com.casamento.matheus;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class MatheusApplicationTests {
+
+}
---
+++ src/test/java/com/example/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,8 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class SpringbootApplicationTests {
+
+}
